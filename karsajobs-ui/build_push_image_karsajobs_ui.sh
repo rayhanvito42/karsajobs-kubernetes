424f50cfa31311760a5de4fdf6c735b50056065d
rayhanvito42 <rayhan.20042@mhs.unesa.ac.id>
2023-11-20 11:51:49 +0700

    first commit

---
+++ karsajobs-ui/build_push_image_karsajobs_ui.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#build docker image from Dockerfile from current directory with name rayhanvito27/karsajobs-ui and tag latest
+docker build -t rayhanvito27/karsajobs-ui:latest .
+echo '\n\n' #print new line to give some space
+
+# create a new image tag from source image, still refer to the same image, but have different name/tag
+docker tag rayhanvito27/karsajobs-ui:latest ghcr.io/rayhanvito27/karsajobs-ui:latest
+echo '\n\n' #print new line to give some space
+
+
+# login to github packages
+echo $GITHUB_PACKAGES_TOKEN | docker login ghcr.io -u rayhanvito42 --password-stdin
+echo '\n\n' #print new line to give some space
+
+# push the built image to github packages
+docker push ghcr.io/rayhanvito42/karsajobs-ui:latest
+echo '\n\n' #print new line to give some space
